c20339aa0161f1502d40b4d8d0c3f788660b43bf
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-27 00:38:04 -0700

    Code Done

---
+++ Switch.js
@@ -0,0 +1,30 @@
+
+const weekName = 4 ;
+
+switch (weekName) {
+  case 1:
+    console.log("monday")
+    break;
+    case 2:
+    console.log("Tuesday")
+    break;
+    case 3:
+    console.log("wednesday")
+    break;
+    case 4:
+    console.log("Thursday")
+    break;
+    case 5:
+    console.log("friday")
+    break;
+    case 6:
+    console.log("saturday")
+    break;
+    case 7:
+    console.log("sunday")
+    break;
+
+  default:
+    console.log("default case march")
+    break;
+}
